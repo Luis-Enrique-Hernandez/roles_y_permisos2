b31266fcfbb8e295c97778e5286ec9a616ee26f5
Luis-Enrique-Hernandez <losdelafehernandez@hotmail.com>
2020-06-24 20:14:16 -0500

    segundo proyecto pero con roles y permisos

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\User;
+use App\Permission\Models\Role;
+use App\Permission\Models\Permission;
+use Illuminate\Support\Facades\Gate;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/role', 'RoleController')->names('role');
+
+Route::resource('/user', 'UserController', ['except'=>[
+    'create','store']])->names('user');
+
+Route::resource('/product', 'ProductController')->names('product');
